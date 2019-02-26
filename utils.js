befa2a831e04460ad940ab1284556cf4c991d00b
likeju <likeju@ksjgs.com>
2019-02-26 10:34:57 +0800

    函数柯里化

---
+++ utils.js
@@ -0,0 +1,18 @@
+function curry (fn, currArgs) {
+    return function() {
+        let args = [].slice.call(arguments);
+
+        // 首次调用时，若未提供最后一个参数currArgs，则不用进行args的拼接
+        if (currArgs !== undefined) {
+            args = args.concat(currArgs);
+        }
+
+        // 递归调用
+        if (args.length < fn.length) {
+            return curry(fn, args);
+        }
+
+        // 递归出口
+        return fn.apply(null, args);
+    }
+}
